ed1b3c24b18ae73b70e0bfe872bf0efbf39fe5e9
Michael Ranaldo <michael@ranaldo.co.uk>
2017-12-12 23:02:51 +0000

    Complete example Jaspic Module

--- src/main/java/uk/co/ranaldo/playground/jaspicauthmodule/JaspicAuthModule.java
+++ src/main/java/uk/co/ranaldo/playground/jaspicauthmodule/JaspicAuthModule.java
@@ -19,73 +19,79 @@ import javax.servlet.http.HttpServletResponse;
  * @author Michael Ranaldo <michael@ranaldo.co.uk>
  */
 public class JaspicAuthModule implements ServerAuthModule {
-    
+
     /**
-     * List the message types which we respond to (ie only Http requests and responses delivered via a servlet).
-     * Hide the warnings from Request and Response as we're dealing with a basic application.
-     * 
+     * List the message types which we respond to (ie only Http requests and responses delivered via a servlet). Hide
+     * the warnings from Request and Response as we're dealing with a basic application.
+     *
      */
     @SuppressWarnings("rawtypes")
-    protected static final Class[] SUPPORTED_MESSAGE_TYPES = new Class[] {
-        HttpServletRequest.class, HttpServletResponse.class };
-    
+    protected static final Class[] SUPPORTED_MESSAGE_TYPES = new Class[]{
+        HttpServletRequest.class, HttpServletResponse.class};
+
     private CallbackHandler handler;
 
     /**
      * Initialize our module (constructor).
+     *
      * @param requestPolicy
      * @param responsePolicy
      * @param handler
      * @param options
-     * @throws AuthException 
+     * @throws AuthException
      */
     @Override
-    public void initialize(MessagePolicy requestPolicy, MessagePolicy responsePolicy, CallbackHandler handler, Map options) throws AuthException {
+    public void initialize(MessagePolicy requestPolicy, MessagePolicy responsePolicy, CallbackHandler handler,
+            Map options) throws AuthException {
         System.out.println("Initialize Called.");
         this.handler = handler;
     }
 
     /**
      * Return the HTTP Servlet Request and Response types.
+     *
      * @return [ request.class, response.class ]
      */
+    @SuppressWarnings("rawtypes")
     @Override
     public Class[] getSupportedMessageTypes() {
         return SUPPORTED_MESSAGE_TYPES;
     }
 
     /**
-     * Main method: In order to pass on the user and role, this example has them as servlet request parameters. 
-     * This method extricates the user and role from the servlet parameters to authenticate them via authenticateUser.
+     * Main method: In order to pass on the user and role, this example has them as servlet request parameters. This
+     * method extricates the user and role from the servlet parameters to authenticate them via authenticateUser.
+     *
      * @param messageInfo
      * @param clientSubject
      * @param serviceSubject
      * @return
-     * @throws AuthException 
+     * @throws AuthException
      */
     @Override
-    public AuthStatus validateRequest(MessageInfo messageInfo, Subject clientSubject, Subject serviceSubject) throws AuthException {
+    public AuthStatus validateRequest(MessageInfo messageInfo, Subject clientSubject, Subject serviceSubject)
+            throws AuthException {
         System.out.println("ValidateRequest Called. ");
         HttpServletRequest request = (HttpServletRequest) messageInfo.getRequestMessage();
-        
+
         String user = request.getParameter("user");
         String group = request.getParameter("group");
-        
-        System.out.println("validateRequest called.");
+
         System.out.println("User = " + user);
         System.out.println("Group = " + group);
-        
+
         authenticateUser(user, group, clientSubject, serviceSubject);
-        
+
         return AuthStatus.SUCCESS;
     }
 
     /**
      * Returns SUCCESS if a response is deemed secure.
+     *
      * @param messageInfo
      * @param serviceSubject
      * @return
-     * @throws AuthException 
+     * @throws AuthException
      */
     @Override
     public AuthStatus secureResponse(MessageInfo messageInfo, Subject serviceSubject) throws AuthException {
@@ -94,9 +100,10 @@ public class JaspicAuthModule implements ServerAuthModule {
 
     /**
      * Clear all principals from the subject.
+     *
      * @param messageInfo
      * @param subject
-     * @throws AuthException 
+     * @throws AuthException
      */
     @Override
     public void cleanSubject(MessageInfo messageInfo, Subject subject) throws AuthException {
@@ -106,16 +113,16 @@ public class JaspicAuthModule implements ServerAuthModule {
     }
 
     /**
-     * 
+     *
      */
     private void authenticateUser(String user, String group, Subject clientSubject, Subject serverSubject) {
         System.out.println("Authenticating user " + user + " in group " + group);
         CallerPrincipalCallback callerPrincipalCallback = new CallerPrincipalCallback(clientSubject, user);
         GroupPrincipalCallback groupPrincipalCallback = new GroupPrincipalCallback(clientSubject, new String[]{group});
-        
+
         try {
-            handler.handle(new Callback[] { callerPrincipalCallback, groupPrincipalCallback });
-        } catch (Exception godException){
+            handler.handle(new Callback[]{callerPrincipalCallback, groupPrincipalCallback});
+        } catch (Exception godException) {
             System.out.println(godException.getMessage());
         }
     }
